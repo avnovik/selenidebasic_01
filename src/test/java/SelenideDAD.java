8d36062c8a339e02ea027fe6b9091fde067844b2
Novikov Aleksei <3900676@gmail.com>
2021-11-04 03:35:40 +0300

    Add DrugAndDrop assertion

--- src/test/java/SelenideDAD.java
+++ src/test/java/SelenideDAD.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.*;
@@ -7,5 +8,8 @@ public class SelenideDAD {
     void drugAndDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(Condition.text("B"));
+        $("#column-b").shouldHave(Condition.text("A"));
+
     }
 }
